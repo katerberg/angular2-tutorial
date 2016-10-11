fd327e88b1f57327706a4e84a8f1327245574f8a
Mark Katerberg <mark.katerberg@rgare.com>
2016-10-11 16:20:24 -0500

    hero service now returns promise of data

--- app/app.component.ts
+++ app/app.component.ts
@@ -76,7 +76,7 @@ export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
