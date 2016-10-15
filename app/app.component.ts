6b648cfbaab7fdf5fce0323248b4ed7deeda820d
Katerberg <katerberg@fastmail.fm>
2016-10-15 00:12:07 -0500

    Makes file compile by defining type of heroes on app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -74,6 +74,7 @@ export class AppComponent implements OnInit {
     this.getHeroes();
   }
   title = 'Tour of Heroes';
+  heroes: Hero[];
   selectedHero: Hero;
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
